16f4ca04c329a3bf2bdcc578b06fbfcd129d950e
Jackie <jackienorstrom@aol.com>
2019-04-15 11:54:27 -0400

    Starting the tabController

---
+++ LASDictionaryV3/MainTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  MainTabBarController.swift
+//  LASDictionaryV3
+//
+//  Created by Jackie on 4/15/19.
+//  Copyright © 2019 LAS. All rights reserved.
+//
+
+import UIKit
+
+
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let favoritesController = ViewController()
+        
+        favoritesController.tabBarItem
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
